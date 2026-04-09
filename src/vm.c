50c0d8e979d8dafa03a7810520893eb1037ee202
Kylee Tilley <kyleetilley@gmail.com>
2026-04-08 20:12:18 -0500

    Implement binary operators

--- src/vm.c
+++ src/vm.c
@@ -36,6 +36,13 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op)     \
+    do                    \
+    {                     \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b);     \
+    } while (false)
 
     for (;;)
     {
@@ -63,6 +70,18 @@ static InterpretResult run()
             push(constant);
             break;
         }
+        case OP_ADD:
+            BINARY_OP(+);
+            break;
+        case OP_SUBTRACT:
+            BINARY_OP(-);
+            break;
+        case OP_MULTIPLY:
+            BINARY_OP(*);
+            break;
+        case OP_DIVIDE:
+            BINARY_OP(/);
+            break;
         case OP_NEGATE:
             push(-pop());
             break;
@@ -77,6 +96,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk)
