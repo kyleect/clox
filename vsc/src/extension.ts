0175f811806003b5045b1d629169aadd9c5a3de7
Kylee Tilley <kyleetilley@gmail.com>
2026-05-02 19:31:35 -0500

    Refactor vsc extension to read hover files in automatically

--- vsc/src/extension.ts
+++ vsc/src/extension.ts
@@ -1,6 +1,8 @@
 import * as vscode from "vscode";
-import { readFileSync } from "node:fs";
+import { readFileSync, existsSync } from "node:fs";
 import { join } from "node:path";
+
+const hoverCache: Record<string, string> = {};
 
 export function activate(context: vscode.ExtensionContext) {
   const EXT_ROOT = context.extensionPath;
@@ -9,84 +11,22 @@ export function activate(context: vscode.ExtensionContext) {
     provideHover(doc, position) {
       const currentWord = doc.getText(doc.getWordRangeAtPosition(position));
 
-      let contents: string = "";
+      if (hoverCache[currentWord]) {
+        return new vscode.Hover(
+          new vscode.MarkdownString(hoverCache[currentWord]),
+        );
+      } else {
+        let hoverText: string = "";
 
-      switch (currentWord) {
-        case "this":
-          contents = readFileSync(join(EXT_ROOT, "hovers", "this.md"), "utf8");
-          break;
-        case "exit":
-          contents = readFileSync(join(EXT_ROOT, "hovers", "exit.md"), "utf8");
-          break;
-        case "argc":
-          contents = readFileSync(join(EXT_ROOT, "hovers", "argc.md"), "utf8");
-          break;
-        case "argv":
-          contents = readFileSync(join(EXT_ROOT, "hovers", "argv.md"), "utf8");
-          break;
-        case "len":
-          contents = readFileSync(join(EXT_ROOT, "hovers", "len.md"), "utf8");
-          break;
-        case "__version__":
-          contents = readFileSync(
-            join(EXT_ROOT, "hovers", "__version__.md"),
-            "utf8",
-          );
-          break;
-        case "setenv":
-          contents = readFileSync(
-            join(EXT_ROOT, "hovers", "setenv.md"),
-            "utf8",
-          );
-          break;
-        case "getenv":
-          contents = readFileSync(
-            join(EXT_ROOT, "hovers", "getenv.md"),
-            "utf8",
-          );
-          break;
-        case "typeof":
-          contents = readFileSync(
-            join(EXT_ROOT, "hovers", "typeof.md"),
-            "utf8",
-          );
-          break;
-        case "instanceOf":
-          contents = readFileSync(
-            join(EXT_ROOT, "hovers", "instanceOf.md"),
-            "utf8",
-          );
-          break;
-        case "stdin":
-          contents = readFileSync(join(EXT_ROOT, "hovers", "stdin.md"), "utf8");
-          break;
-        case "prompt":
-          contents = readFileSync(
-            join(EXT_ROOT, "hovers", "prompt.md"),
-            "utf8",
-          );
-          break;
-        case "readFileToString":
-          contents = readFileSync(
-            join(EXT_ROOT, "hovers", "readFileToString.md"),
-            "utf8",
-          );
-          break;
-        case "fileExists":
-          contents = readFileSync(
-            join(EXT_ROOT, "hovers", "fileExists.md"),
-            "utf8",
-          );
-          break;
-        case "ceil":
-          contents = readFileSync(join(EXT_ROOT, "hovers", "ceil.md"), "utf8");
-          break;
-        case "clock":
-          contents = readFileSync(join(EXT_ROOT, "hovers", "clock.md"), "utf8");
-          break;
+        const hoverFilePath = join(EXT_ROOT, "hovers", `${currentWord}.md`);
+
+        if (existsSync(hoverFilePath)) {
+          hoverText = readFileSync(hoverFilePath, "utf8");
+          hoverCache[currentWord] = hoverText;
+        }
+
+        return new vscode.Hover(new vscode.MarkdownString(hoverText));
       }
-
-      return new vscode.Hover(new vscode.MarkdownString(contents));
     },
   });
 
